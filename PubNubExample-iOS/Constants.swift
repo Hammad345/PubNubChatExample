25a2fc0d3f36fb483bc7e3579cc50eff3e9df379
Syed Hammad Jan <hammad.hbl2009@gmail.com>
2026-03-04 15:47:00 +0500

    Update Constants.swift
    updated code

--- PubNubExample-iOS/Constants.swift
+++ PubNubExample-iOS/Constants.swift
@@ -7,5 +7,5 @@
 
 import Foundation
 
-let pubNubPublishKey = "pub-c-d905976a-ccc3-4907-baeb-01ad66abefcf"
-let pubNubSubscribeKey = "sub-c-011a8f66-34a4-11e7-81b3-02ee2ddab7fe"
+let pubNubPublishKey = ""
+let pubNubSubscribeKey = ""
